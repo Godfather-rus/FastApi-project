529a8b1e2dc408b84559d54e2409545661dee5b5
Godfather <Godfather_russia@mail.ru>
2024-09-27 23:31:25 +0300

    Settimo lezione

--- main.py
+++ main.py
@@ -1,124 +1,66 @@
-from fastapi import FastAPI, HTTPException, Path, Query, Body
+from fastapi import FastAPI, HTTPException, Path, Query, Body, Depends
 from typing import Optional, List, Dict, Annotated
-from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+
+from fastapi.middleware.cors import CORSMiddleware
+from models import Base, User, Post
+from database import engine, session_local
+from schemas import UserCreate, PostCreate, PostResponse, User as DBUser
 
 app = FastAPI()
 
-
-class User(BaseModel):
-    id: int
-    name: str
-    age: int
-
-
-class Post(BaseModel):
-    id: int
-    title: str
-    body: str
-    author: User
-
-
-class PostCreate(BaseModel):
-    title: str
-    body: str
-    author_id: int
-
-
-class UserCreate(BaseModel):
-    name: Annotated[
-        str, Field(..., title='Имя пользователя', min_length=2, max_length=20)
-    ]
-    age: Annotated[
-        int, Field(..., title='Возраст пользователя', ge=1, le=120)
-    ]
-
-users = [
-    {'id': 1, 'name': 'John', 'age': 14},
-    {'id': 2, 'name': 'Bob', 'age': 22},
-    {'id': 3, 'name': 'Alex', 'age': 33}
+origins = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
 ]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@app.get('/')
-def home() -> dict[str, str]:
-    return {'data': 'hello'}
-
-
-@app.get('/contacts')
-def contacts() -> int:
-    return 34
-
-
-posts = [
-    {'id': 1, 'title': 'News 1', 'body': 'Text 1', 'author': users[0]},
-    {'id': 2, 'title': 'News 2', 'body': 'Text 2', 'author': users[2]},
-    {'id': 3, 'title': 'News 3', 'body': 'Text 3', 'author': users[1]},
-]
+Base.metadata.create_all(bind=engine)
 
 
-# @app.get('/items')
-# async def items() -> List[Post]:
-#     post_objects = []
-#     for post in posts:
-#         post_objects.append(Post(id=post['id'], title=post['title'], body=post['body']))
-#     return post_objects
-@app.get('/items')
-async def items() -> List[Post]:
-    return [Post(**post) for post in posts]
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.post('/items/add')
-async def add_item(post: PostCreate) -> Post:
-    author = next((user for user in users if user['id'] == post.author_id), None)
-    if not author:
-        raise HTTPException(status_code=404, detail='Author not found')
+@app.post('/users/', response_model=DBUser)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)) -> DBUser:
+    db_user = User(name=user.name, age=user.age)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
 
-    new_post_id = len(posts) + 1
-
-    new_post = {'id': new_post_id, 'title': post.title, 'body': post.body, 'author': author}
-
-    posts.append(new_post)
-
-    return Post(**new_post)
+    return db_user
 
 
-@app.post('/user/add')
-async def add_user(user: Annotated[
-    UserCreate,
-    Body(..., example={
-        'name': 'UserName',
-        'age': '1'
-    })
-]) -> User:
+@app.post('/posts/', response_model=PostResponse)
+async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> PostResponse:
+    db_user = db.query(User).filter(User.id == post.author_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
 
-    new_user_id = len(users) + 1
+    db_post = Post(title=post.title, body=post.body, author_id=post.author_id)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
 
-    new_user = {'id': new_user_id, 'name': user.name, 'age': user.age}
-
-    posts.append(new_user)
-
-    return User(**new_user)
+    return db_post
 
 
-@app.get('/items/{id}')
-def items(id: Annotated[int, Path(..., title='Здесь указывается id поста', ge=1, lt=100)]) -> Post:
-    for post in posts:
-        if post['id'] == id:
-            return Post(**post)
+@app.get('/posts/', response_model=List[PostResponse])
+async def posts(db: Session = Depends(get_db)):
+    return db.query(Post).all()
 
-    raise HTTPException(status_code=404, detail='Post not found')
-
-
-@app.get('/search')
-def search(post_id: Annotated[
-    Optional[int],
-    Query(title='ID of post to search for', ge=1, le=50)
-]) -> Dict[str, Optional[Post]]:
-    if post_id:
-        for post in posts:
-            if post['id'] == post_id:
-                return {'data': Post(**post)}
-
-        raise HTTPException(status_code=404, detail='Post not found')
-    else:
-        return {'data': None}
+@app.get('/users/', response_model=List[PostResponse])
+async def posts(db: Session = Depends(get_db)):
+    return db.query(Post).all()
---
+++ schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    name: str
+    age: int
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class PostBase(BaseModel):
+    title: str
+    body: str
+    author_id: int
+
+
+class PostResponse(PostBase):
+    id: int
+    author: User
+
+    class Config:
+        orm_mode = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+
